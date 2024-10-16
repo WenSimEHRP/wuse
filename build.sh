38b49af938888387a4f2c74a4aa207397db6cefb
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-10-15 22:10:41 -0700

    Change: use smaller action1s

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-/home/jeremy/Documents/code/wers/.venv/bin/python /home/jeremy/Documents/code/wers/src/station.py &&
+.venv/bin/python src/station.py &&
     ../yagl/build/yagl -d station.grf &&
-    rm -r station &&
-    grftopy station.grf &&
+    echo "$(date -u '+%Y-%m-%d %H:%M:%S %z') $(md5sum station.grf)" >> build.log &&
     cp station.grf ~/.local/share/openttd/newgrf &&
     echo "copied"
--- src/station.py
+++ src/station.py
@@ -1,13 +1,14 @@
 import grf
 
-road_ground_tiles = {
-    "bay_ne": 0xA84,
-    "bay_se": 0xA85,
-    "bay_nw": 0xA86,
-    "bay_sw": 0xA87,
-    "pass_ne_sw": 0x522,
-    "pass_nw_se": 0x521,
-}
+road_ground_tiles = (
+    ("bay_ne", 0xA84),
+    ("bay_se", 0xA85),
+    ("bay_nw", 0xA86),
+    ("bay_sw", 0xA87),
+    ("pass_ne_sw", 0x522),
+    ("pass_nw_se", 0x521),
+)
+
 
 class LayoutOperation:
     @staticmethod
@@ -62,9 +63,19 @@ class LayoutOperation:
                 )
         raise ValueError(f"Invalid axis: {axis}")
 
+    @staticmethod
+    def calculate_offset(x, y):
+        # Convert x and y to their respective nibbles
+        high_nibble = x & 0x0F
+        low_nibble = y & 0x0F
+
+        # Combine the high and low nibbles into a single byte
+        offset = (high_nibble << 4) | low_nibble
+
+        return offset
+
 
 class SequenceManager:
-
     def __init__(self) -> None:
         self.labels = {}
         self.new_len = 0
@@ -73,9 +84,9 @@ class SequenceManager:
         self.labels[s] = self.labels.get(s, len(self.labels))
         return self.labels[s]
 
-    def new(self) -> int:
+    def new(self, i=1) -> int:
         current_len = self.new_len
-        self.new_len += 1
+        self.new_len += i
         return current_len
 
 class StringManager(SequenceManager):
@@ -89,7 +100,6 @@ class StringManager(SequenceManager):
 
 class_string_manager = StringManager()
 
-
 class RoadStop(grf.SpriteGenerator):
     FEATURE = grf.ROAD_STOP
 
@@ -100,15 +110,37 @@ class RoadStop(grf.SpriteGenerator):
         self.callbacks = grf.make_callback_manager(grf.ROAD_STOP, callbacks)
         self.id: int = id
         self.name: str = name
-        self.sprite_layouts: list[grf.AdvancedSpriteLayout] = sprite_layouts
+        self.sprite_layouts: dict[str, grf.AdvancedSpriteLayout] = sprite_layouts
         self.class_label: bytes = grf.to_bytes(class_label)
         self.class_name: str = class_name
         self._props = props
 
     def get_sprites(self, g) -> list:
+        raise NotImplementedError
+
+class RoadDeco(RoadStop):
+    def __init__(
+        self,
+        callbacks,
+        class_name,
+        class_label,
+        sprites: list[grf.FileSprite],
+        name,
+        props={},
+        id=None,
+    ):
+        super().__init__(callbacks, props, class_name, class_label, None, name, id)
+        self.sprites = sprites
+
+    def get_sprites(self, g) -> list:
         res = []  # to return this thing
 
-        res.extend(self.sprite_layouts)
+        res.append(
+            grf.Action1(
+                feature=grf.ROAD_STOP, set_count=1, sprite_count=len(self.sprites), first_set=0
+            )
+        )
+        res.extend(self.sprites)
 
         # strings
         for s in (self.name, self.class_name):
@@ -131,45 +163,88 @@ class RoadStop(grf.SpriteGenerator):
                     "class_label": self.class_label,
                     "name_id": class_string_manager.index(self.name),
                     "class_name_id": class_string_manager.index(self.class_name),
+                    "general_flags": 0b00001000,
+                    "draw_mode": 4,
                     **self._props,
                 },
             )
         )
 
+        layouts = {
+            i: grf.AdvancedSpriteLayout(
+                feature=grf.ROAD_STOP,
+                ground={
+                    "sprite": grf.SpriteRef(
+                        id=val[1],
+                        pal=0,
+                        is_global=True,
+                        use_recolour=False,
+                        always_transparent=False,
+                        no_transparent=False,
+                    ),
+                },
+                buildings=[
+                    {
+                        "sprite": grf.SpriteRef(
+                            id=0,
+                            pal=0,
+                            is_global=False,
+                            use_recolour=False,
+                            always_transparent=False,
+                            no_transparent=False,
+                        ),
+                        "add": grf.Temp(0),
+                    },
+                ]
+            )
+            for i, val in enumerate(road_ground_tiles)
+            if i in (4, 5)
+        }
+
         res.append(
             layout := grf.Switch(
                 feature=self.FEATURE,
                 related_scope=False,
-                code=("view"),
-                ranges={i: res[i] for i in range(6)},
-                default = res[2],
+                code=(
+                    "TEMP[1] = formation()",
+                    "TEMP[0] = view - 4",
+                    "view"),
+                ranges={**layouts},
+                default=next(iter(layouts.values()), None),
+                subroutines={
+                    "formation": grf.Switch(
+                        feature=self.FEATURE,
+                        related_scope=False,
+                        ranges={},
+                        code=(f"var(0x68, param=({LayoutOperation.calculate_offset(0,-1)}), shift=0, and=0x8) == var(0x68, param=({LayoutOperation.calculate_offset(0,0)}), shift=0, and=0x8)"),
+                        default=0,
+                    )
+                }
             )
         )
 
         res.append(
             grf.Map(
                 definition,
-                {},  # cargo types
-                layout,  # FIXME
+                {},
+                layout,
             )
         )
 
         return res
 
-class RoadDeco(RoadStop):
-    def __init__(self, callbacks, props, class_name, class_label, sprite_layouts, name, id=None):
-        super().__init__(callbacks, props, class_name, class_label, sprite_layouts, name, id)
 
 def tmpl_pullouts(x, func):
-    return [
-        func(72 * x + 1, 35 * i + 1, 64, 31, xofs=-31, yofs=0) for i in range(16)
-    ]
-
+    ls = []
+    for i in range(8):
+        ls.append(func(72 * x + 1, 35 * i + 1, 64, 31, xofs=-31, yofs=0))
+        ls.append(func(72 * x + 1, 35 * (i + 8) + 1, 64, 31, xofs=-31, yofs=0))
+    return ls
 
 pullouts_png = grf.ImageFile("gfx/u-ratt-pullouts.png")  # TODO finish
 sprites = [
     sprite
-    for i in range(6)
+    for i in range(7)
     for sprite in tmpl_pullouts(
         i, lambda *args, **kw: grf.FileSprite(pullouts_png, *args, **kw, bpp=8)
     )
@@ -181,63 +256,18 @@ g = grf.NewGRF(
     description="grf-py road station sample",
 )
 
-
-g.add(
-    grf.Action1(
-        feature=grf.ROAD_STOP,
-        set_count=len(sprites),
-        sprite_count=1,
-        first_set=None,
-    )
-)
-g.add(*sprites)
-
-def call_next():
-    processed = {}
-    yield len(processed)
-
-sequence_manager = SequenceManager()
 id_manager = SequenceManager()
 
-stops = [RoadStop(
-    callbacks=None,
-    props={},
-    class_name="Station",
-    class_label=b"STAT",
-    name="Sample station",
-    id=id_manager.new(),
-
-    sprite_layouts=[
-        grf.AdvancedSpriteLayout(
-            feature=grf.ROAD_STOP,
-            ground={
-                "sprite": grf.SpriteRef(
-                    id=gfx,
-                    pal=0,
-                    is_global=True,
-                    use_recolour=False,
-                    always_transparent=False,
-                    no_transparent=False,
-                ),
-            },
-            buildings=[
-                {
-                    "sprite": grf.SpriteRef(
-                        id=sequence_manager.new(),
-                        pal=0,
-                        is_global=False,
-                        use_recolour=False,
-                        always_transparent=False,
-                        no_transparent=False,
-                    ),
-                    "offset": (0, 0, 0),
-                    "extent": (16,16,0),
-                }
-            ]
-        ) for gfx in (0xA84, 0xA85, 0xA86, 0xA87, 0x522, 0x521)
-    ],
-) for i in range(len(sprites)//6+1)]
-
+stops = [
+    RoadDeco(
+        callbacks=None,
+        class_name="Station",
+        class_label=b"STAT",
+        name="Sample station",
+        id=id_manager.new(), # wow new id!
+        sprites=[sprite1, sprite2],
+    ) for sprite1, sprite2 in zip(sprites[::2], sprites[1::2])
+]
 
 g.add(*stops)
 g.write("station.grf")
